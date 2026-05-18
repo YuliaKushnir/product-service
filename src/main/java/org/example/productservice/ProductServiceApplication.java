5adc764c568f1f7e47e05f838a24ade6cc2602e7
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-05-18 21:33:02 +0300

    Changed deploy settings

--- src/main/java/org/example/productservice/ProductServiceApplication.java
+++ src/main/java/org/example/productservice/ProductServiceApplication.java
@@ -8,14 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
-
         Dotenv dotenv = Dotenv.load();
-        System.setProperty("spring_datasource_username", dotenv.get("spring_datasource_username"));
-        System.setProperty("spring_datasource_password", dotenv.get("spring_datasource_password"));
-        System.setProperty("nebius", dotenv.get("nebius"));
-        System.setProperty("cloudinary_cloud-name", dotenv.get("cloudinary_cloud-name"));
-        System.setProperty("cloudinary_api-key", dotenv.get("cloudinary_api-key"));
-        System.setProperty("cloudinary_api_secret", dotenv.get("cloudinary_api_secret"));
+        System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+        System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+        System.setProperty("CLOUDINARY_CLOUD_NAME", dotenv.get("CLOUDINARY_CLOUD_NAME"));
+        System.setProperty("CLOUDINARY_API_KEY", dotenv.get("CLOUDINARY_API_KEY"));
+        System.setProperty("CLOUDINARY_API_SECRET", dotenv.get("CLOUDINARY_API_SECRET"));
 
         SpringApplication.run(ProductServiceApplication.class, args);
     }
