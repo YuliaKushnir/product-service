a4c3b189e35edd2820e2fec725e0149f66d4c7af
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-05-22 10:27:40 +0300

    temporary

--- src/main/java/org/example/productservice/config/SecurityConfig.java
+++ src/main/java/org/example/productservice/config/SecurityConfig.java
@@ -58,7 +58,8 @@ public class SecurityConfig {
 
                         .requestMatchers("/actuator/**").permitAll()
 
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
+//                        .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt ->
