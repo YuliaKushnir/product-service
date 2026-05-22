768b9317bf5de24e9e4ae812b6429f7c78896bbd
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-05-22 14:07:02 +0300

    temp

--- src/main/java/org/example/productservice/config/WebConfig.java
+++ src/main/java/org/example/productservice/config/WebConfig.java
@@ -5,24 +5,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
-@Configuration
+//@Configuration
 public class WebConfig {
 
-    @Value("${frontend-url}")
-    private String frontendUrl;
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-
-                registry.addMapping("/**")
-                        .allowedOriginPatterns("https://34.116.235.108")
-                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+//    @Value("${frontend-url}")
+//    private String frontendUrl;
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//
+//                registry.addMapping("/**")
+//                        .allowedOriginPatterns("https://34.116.235.108")
+//                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+//                        .allowedHeaders("*")
+//                        .allowCredentials(true);
+//            }
+//        };
+//    }
 }
