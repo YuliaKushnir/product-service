fc860ef1561b98b1f4772b65a2df1f9a70fc09bd
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2026-05-24 09:11:49 +0300

    added jwt decoder

--- src/main/java/org/example/productservice/config/WebConfig.java
+++ src/main/java/org/example/productservice/config/WebConfig.java
@@ -1,28 +1,24 @@
 package org.example.productservice.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
-//@Configuration
+@Configuration
 public class WebConfig {
 
-//    @Value("${frontend-url}")
-//    private String frontendUrl;
-//
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//
-//                registry.addMapping("/**")
-//                        .allowedOriginPatterns("https://34.116.235.108")
-//                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-//                        .allowedHeaders("*")
-//                        .allowCredentials(true);
-//            }
-//        };
-//    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("https://34.116.235.108")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
